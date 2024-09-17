109d92f9660b57bc312b05e8ee509ca1c5a81877
Garvit Singh Chauhan <124265043+Garvitschauhan@users.noreply.github.com>
2024-09-17 23:23:51 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Garvit Singh Chauhan",
+  title: "Web Designer & Software Developer",
+  email: "Garvitschauhan@gmail.com",
+  gitHub: "Garvitschauhan",
+  linkedIn: "Garvitschauhan",
 };
 
 const primaryColor = "#4E567E";
